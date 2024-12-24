b3c193068840c4744bf18f2493b5dafaddcc4934
Lautaro Casal <Laucasal27@gmail.com>
2024-12-24 15:15:37 -0300

    Add files via upload

--- Web Programming/backend/categorias.php
+++ Web Programming/backend/categorias.php
@@ -1,1 +1,17 @@
+<?php
 
+include '../class/autoload.php';
+
+if(isset($_POST['accion']) && $_POST['accion'] == 'guardar') {
+    $miCategoria = new Categorias();
+    $miCategoria->nombre = $_POST['categoria'];
+    $miCategoria->guardar();
+} elseif (isset($_GET['add'])) {
+    include 'views/categorias.html';
+    die();
+}
+
+$lista_ctg = Categorias::listar();
+include 'views/lista_categorias.html';
+
+?>
---
+++ Web Programming/backend/productos.php
@@ -0,0 +1,20 @@
+<?php
+
+include '../class/autoload.php';
+
+if(isset($_POST['accion']) && $_POST['accion'] === 'guardar') {
+    $miproducto = new Productos();
+    $miproducto->nombre = $_POST['nombre'];    
+    $miproducto->descripcion = $_POST['descripcion'];
+    $miproducto->precio = $_POST['precio'];    
+    $miproducto->categoria = $_POST['categoria'];
+    $miproducto->guardar();
+} elseif (isset($_GET['add'])) {
+    include 'views/productos.html';
+    die();
+}
+
+$lista_pro = Productos::listar();
+include 'views/lista_productos.html';
+
+?>
